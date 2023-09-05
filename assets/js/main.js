989cd853c6266fe818a5cdb4976c199d1db95342
sanjida123shammi <sanjida717270@gmail.com>
2023-09-05 13:32:41 +0600

    992px to 1199px responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
         margin:10,
         nav:true,
         dots:true,
-        autoplay:true,
+        autoplay:false,
         autoplayTimeout:4000,
         smartSpeed:750,
         responsive:{
